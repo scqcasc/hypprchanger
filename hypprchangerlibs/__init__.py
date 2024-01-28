70a2f429e706c0d5dcc3ad39c79d7537004a0389
Sean Quaint <seanquaint@zohomail.com>
2024-01-28 07:44:31 -0500

    updated docs

--- hypprchangerlibs/__init__.py
+++ hypprchangerlibs/__init__.py
@@ -7,4 +7,16 @@ Provides several functions to dynamically manage wallpapers and monitors in Hypr
 * set_wallpaper_on_activeworkspace
 * find_cmd
 * check_environment
+* process_ipc reads json input to send commands to the hypprserver.  Currently only one command is supported which is halt.
+  The json structure for this is 
+
+    "commands": {
+        "execute": [
+            "halt"
+        ]
+    }
+}
+* read_ipc loads the json input for process_ipc
+* cleanup_ipc removes the ipc file after processing
+
 """
